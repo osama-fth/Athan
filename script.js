8567d05de2557f9f5380f2ccec680005d6365da6
Osama Foutih <foutihosama@gmail.com>
2025-03-07 20:36:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -375,7 +375,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         // Se i dati sono scaduti o non esistono nella cache, facciamo la richiesta all'API
-        const apiUrl = `https://api.aladhan.com/v1/timings/${date}?latitude=${city.lat}&longitude=${city.lng}&method=99&methodSettings=12.5,null,0&adjustments=0,0,0,0,0,0,90`;
+        const apiUrl = `https://api.aladhan.com/v1/timings/${date}?latitude=${city.lat}&longitude=${city.lng}&method=99&methodSettings=12.5,null,null&shafaq=general&isha=90`;
 
         fetch(apiUrl)
             .then(response => response.json())
